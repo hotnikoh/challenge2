febde355e2bc4f18e8d7c6b7f9fa2330facab5b9
Hotniko Hildebrandus <htniko93@gmail.com>
2023-09-01 20:55:01 +0700

    ch4

--- fahrtocurrying.js
+++ fahrtocurrying.js
@@ -10,16 +10,3 @@ let celcius = 20;
 let convrtCelcFahr = celciusTofahrenheit(celcius);
 console.log(convrtCelcFahr())
 
-//===================================
-
-// function celciusToConverter(celsius) {
-//     return function() {
-//         let celcToFahrn = (celsius * 9/5) + 32;
-//         return celcToFahrn;
-//     };
-// }
-
-// const celcius = 20;
-// const convertToCelsius = celciusToConverter(celcius);
-
-// console.log(convertToCelsius());
